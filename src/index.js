0ab2ed4da71aae5675b45a2e3ae5392dc6dc0034
Entwolker <binxkevin@gmx.de>
2021-03-09 13:25:06 +0100

    Clear & Status Command

--- src/index.js
+++ src/index.js
@@ -9,6 +9,26 @@ client.on('ready', () => {
     command(client, 'ping', (message) => {
         message.channel.send('Pong!')
     })
+
+    command(client, ['cc', 'clear'], (message) => {
+        if (message.member.hasPermission('MANAGE_MESSAGES')) {
+            message.channel.messages.fetch().then((results) => {
+                message.channel.bulkDelete(results)
+            })
+        }
+    });
+
+    command(client, 'status', (message) => {
+        const content = message.content.replace('.status ', '')
+        if (message.member.hasPermission('ADMINISTRATOR')) {
+            client.user.setPresence({
+                activity: {
+                    name: content,
+                    type: 0
+                }
+            })
+        }
+    })
 })
 
 client.login(config.token)
